7736e4c41a82ec47cee6748c0e3ebc72c92400b6
gabrielataras <57159988+gabrielataras@users.noreply.github.com>
2023-10-27 19:11:06 -0400

    Create smartContract.jsx
    Hackathon

---
+++ apps/appname/widget/smartContract.jsx
@@ -0,0 +1,293 @@
+// FETCH LIDO ABI
+
+const lidoContract = "0xae7ab96520de3a18e5e111b5eaab095312d7fe84";
+const mainnetLidoContract = "0xae7ab96520de3a18e5e111b5eaab095312d7fe84";
+const rinkebyLidoContract = "0xF4242f9d78DB7218Ad72Ee3aE14469DBDE8731eD";
+const kovanLidoContract = "0x4b7FCBC11BB45075b9A1F953128C09bC97D6a0D7";
+const gorliLidoContract = "0x1643E812aE58766192Cf7D2Cf9567dF2C37e9B7F";
+const tokenDecimals = 18;
+
+const network = "gorli"; // "gorli" // "rinkeby" // "mainnet"
+
+switch (network) {
+  case "kovan":
+    lidoContract = kovanLidoContract;
+    break;
+  case "gorli":
+    lidoContract = gorliLidoContract;
+    break;
+  case "rinkeby":
+    lidoContract = rinkebyLidoContract;
+    break;
+  case "mainnet":
+    lidoContract = mainnetLidoContract;
+    break;
+  default:
+    lidoContract = mainnetLidoContract;
+    break;
+}
+
+const lidoAbi = fetch(
+  "https://raw.githubusercontent.com/lidofinance/lido-subgraph/master/abis/Lido.json"
+);
+
+if (!lidoAbi.ok) {
+  return "Loading";
+}
+
+const iface = new ethers.utils.Interface(lidoAbi.body);
+
+// FETCH LIDO STAKING APR
+
+if (state.lidoArp === undefined) {
+  const apr = fetch(
+    "https://api.allorigins.win/get?url=https://stake.lido.fi/api/sma-steth-apr"
+  );
+  if (!apr) return;
+  State.update({ lidoArp: JSON.parse(apr?.body?.contents) ?? "..." });
+}
+
+// HELPER FUNCTIONS
+
+const getStakedBalance = (receiver) => {
+  const encodedData = iface.encodeFunctionData("balanceOf", [receiver]);
+
+  return Ethers.provider()
+    .call({
+      to: lidoContract,
+      data: encodedData,
+    })
+    .then((rawBalance) => {
+      const receiverBalanceHex = iface.decodeFunctionResult(
+        "balanceOf",
+        rawBalance
+      );
+
+      return Big(receiverBalanceHex.toString())
+        .div(Big(10).pow(tokenDecimals))
+        .toFixed(2)
+        .replace(/d(?=(d{3})+.)/g, "$&,");
+    });
+};
+
+const submitEthers = (strEther, _referral) => {
+  if (!strEther) {
+    return console.log("Amount is missing");
+  }
+  const erc20 = new ethers.Contract(
+    lidoContract,
+    lidoAbi.body,
+    Ethers.provider().getSigner()
+  );
+
+  let amount = ethers.utils.parseUnits(strEther, tokenDecimals).toHexString();
+
+  erc20.submit(lidoContract, { value: amount }).then((transactionHash) => {
+    console.log("transactionHash is " + transactionHash);
+  });
+};
+
+// DETECT SENDER
+
+if (state.sender === undefined) {
+  const accounts = Ethers.send("eth_requestAccounts", []);
+  if (accounts.length) {
+    State.update({ sender: accounts[0] });
+    console.log("set sender", accounts[0]);
+  }
+}
+
+// FETCH SENDER BALANCE
+
+if (state.balance === undefined && state.sender) {
+  Ethers.provider()
+    .getBalance(state.sender)
+    .then((balance) => {
+      State.update({ balance: Big(balance).div(Big(10).pow(18)).toFixed(2) });
+    });
+}
+
+// FETCH TX COST // only applies to lido
+
+if (state.txCost === undefined) {
+  const gasEstimate = ethers.BigNumber.from(1875000);
+  const gasPrice = ethers.BigNumber.from(1500000000);
+
+  const gasCostInWei = gasEstimate.mul(gasPrice);
+  const gasCostInEth = ethers.utils.formatEther(gasCostInWei);
+
+  // change to uniswap
+  let responseGql = fetch(
+    "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        query: `{
+                      bundle(id: "1" ) {
+                        ethPrice
+                      }
+                    }`,
+      }),
+    }
+  );
+
+  if (!responseGql) return "";
+
+  const ethPriceInUsd = responseGql.body.data.bundle.ethPrice;
+
+  const txCost = Number(gasCostInEth) * Number(ethPriceInUsd);
+
+  State.update({ txCost: txCost.toFixed(2) });
+}
+
+// FETCH CSS
+
+const cssFont = fetch(
+  "https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800"
+).body;
+const css = fetch(
+  "https://pluminite.mypinata.cloud/ipfs/Qmboz8aoSvVXLeP5pZbRtNKtDD3kX5D9DEnfMn2ZGSJWtP"
+).body;
+
+if (!cssFont || !css) return "";
+
+if (!state.theme) {
+  State.update({
+    theme: styled.div`
+                font-family: Manrope, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+                ${cssFont}
+                ${css}
+            `,
+  });
+}
+const Theme = state.theme;
+
+// OUTPUT UI
+
+const getSender = () => {
+  return !state.sender
+    ? ""
+    : state.sender.substring(0, 6) +
+        "..." +
+        state.sender.substring(state.sender.length - 4, state.sender.length);
+};
+
+return (
+  <Theme>
+    <div class="LidoContainer">
+      <div class="Header">🦄 Swap on Uniswap </div>
+      <div class="SubHeader">Swap Using Uniswap v2 Contracts.</div>
+
+      <div class="LidoForm">
+        {state.sender && (
+          <>
+            <div class="LidoFormTopContainer">
+              <div class="LidoFormTopContainerLeft">
+                <div class="LidoFormTopContainerLeftContent1">
+                  <div class="LidoFormTopContainerLeftContent1Container">
+                    <span>Available to stake</span>
+                    <div class="LidoFormTopContainerLeftContent1Circle" />
+                  </div>
+                </div>
+                <div class="LidoFormTopContainerLeftContent2">
+                  <span>
+                    {state.balance ?? (!state.sender ? "0" : "...")}&nbsp;ETH
+                  </span>
+                </div>
+              </div>
+              <div class="LidoFormTopContainerRight">
+                <div class="LidoFormTopContainerRightContent1">
+                  <div class="LidoFormTopContainerRightContent1Text">
+                    <span>{getSender()}</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="LidoSplitter" />
+          </>
+        )}
+        <div
+          class={
+            state.sender ? "LidoFormBottomContainer" : "LidoFormTopContainer"
+          }
+        >
+          <div class="LidoFormTopContainerLeft">
+            <div class="LidoFormTopContainerLeftContent1">
+              <div class="LidoFormTopContainerLeftContent1Container">
+                <span>Staked amount</span>
+              </div>
+            </div>
+            <div class="LidoFormTopContainerLeftContent2">
+              <span>
+                {state.stakedBalance ?? (!state.sender ? "0" : "...")}
+                &nbsp;stETH
+              </span>
+            </div>
+          </div>
+          <div class="LidoFormTopContainerRight">
+            <div class="LidoAprContainer">
+              <div class="LidoAprTitle">Lido APR</div>
+              <div class="LidoAprValue">{state.lidoArp ?? "..."}%</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="LidoStakeForm">
+        <div class="LidoStakeFormInputContainer">
+          <span class="LidoStakeFormInputContainerSpan1"></span>
+          <span class="LidoStakeFormInputContainerSpan2">
+            <input
+              disabled={!state.sender}
+              class="LidoStakeFormInputContainerSpan2Input"
+              value={state.strEther}
+              onChange={(e) => State.update({ strEther: e.target.value })}
+              placeholder="Amount"
+            />
+          </span>
+          <span
+            class="LidoStakeFormInputContainerSpan3"
+            onClick={() => {
+              State.update({
+                strEther: (parseFloat(state.balance) - 0.05).toFixed(2),
+              });
+            }}
+          >
+            <button
+              class="LidoStakeFormInputContainerSpan3Content"
+              disabled={!state.sender}
+            >
+              <span class="LidoStakeFormInputContainerSpan3Max">MAX</span>
+            </button>
+          </span>
+        </div>
+        {!!state.sender ? (
+          <button
+            class="LidoStakeFormSubmitContainer"
+            onClick={() => submitEthers(state.strEther, state.sender)}
+          >
+            <span>Swap</span>
+          </button>
+        ) : (
+          <Web3Connect
+            className="LidoStakeFormSubmitContainer"
+            connectLabel="Connect with Web3"
+          />
+        )}
+
+        <div class="LidoFooterContainer">
+          {state.sender && (
+            <div class="LidoFooterRaw">
+              <div class="LidoFooterRawLeft">You will receive</div>
+              <div class="LidoFooterRawRight">${state.strEther ?? 0} stETH</div>
+            </div>
+          )}
+          <div class="LidoFooterRaw">
+            <div class="LidoFooterRawLeft">Transaction cost</div>
+            <div class="LidoFooterRawRight">{state.txCost}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </Theme>
+);
